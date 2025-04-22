f8d1e4d8f79527c40b9cb92390f420337c13e7e0
Santiago Idarraga Ceballos <san200526@gmail.com>
2025-04-22 12:38:03 -0500

    Added random quote generator

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const phrases = [
+    "Get ready to be inspired…", 
+    "See rejection as redirection.",
+    "There is beauty in simplicity.",
+    "You can’t be late until you show up.",
+    "Maybe life is testing you. Don’t give up.",
+    "Impossible is just an opinion.",
+    "Alone or not you gonna walk forward.",
+]  
+
 app.get('/', (req, res) => {
-    res.send("Hello World") 
+    const number = Math.floor(Math.random() * 7)
+    res.send(phrases[number])
 })
 
 app.listen(port, () => {
